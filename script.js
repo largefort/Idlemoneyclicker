4362a9bb51edf146873461eabb0458c4f80d76f5
largefort <106451813+largefort@users.noreply.github.com>
2023-07-19 16:49:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,122 +1,38 @@
-let earnings = 0;
-let cameraQuality = 1;
-let streamingSoftware = 1;
-let viewerInteraction = 1;
-let streamerName = "";
+// Game state variables
+let money = 0;
+let uploadCount = 0;
+let uploadSpeed = 1;
+let upgradeCost = 10;
 
-// Check if the game data exists in localStorage
-const savedData = JSON.parse(localStorage.getItem("idleStreamerData"));
-
-// Load existing data or show the streamer creator screen
-if (savedData) {
-    loadGameData(savedData);
-    showGamePage();
-} else {
-    showStreamerCreator();
+// Function to upload a file
+function upload() {
+  uploadCount += uploadSpeed;
+  updateUI();
 }
 
-// Function to create a new streamer and start the game
-const createStreamer = () => {
-    streamerName = document.getElementById("streamerNameInput").value;
-    if (streamerName.trim() === "") {
-        alert("Please enter a valid streamer name.");
-        return;
-    }
-    hideStreamerCreator();
-    showGamePage();
-    saveGameData();
-};
+// Function to buy an upload speed upgrade
+function buyUpgrade() {
+  if (money >= upgradeCost) {
+    money -= upgradeCost;
+    uploadSpeed++;
+    upgradeCost *= 2;
+    updateUI();
+  }
+}
 
-const showStreamerCreator = () => {
-    document.getElementById("gamePage").style.display = "none";
-    document.getElementById("streamerCreator").style.display = "block";
-};
+// Function to update the UI with the current game state
+function updateUI() {
+  document.getElementById("upload-count").textContent = uploadCount;
+  document.getElementById("upload-speed").textContent = uploadSpeed;
+  document.getElementById("money").textContent = money;
+  document.getElementById("upgrade-cost").textContent = upgradeCost;
+}
 
-const hideStreamerCreator = () => {
-    document.getElementById("streamerCreator").style.display = "none";
-};
+// Function to handle income generation
+function generateIncome() {
+  money += uploadCount;
+  updateUI();
+}
 
-const showGamePage = () => {
-    document.getElementById("gamePage").style.display = "block";
-};
-
-const updateEarningsDisplay = () => {
-    document.getElementById('earnings').textContent = earnings.toFixed(2);
-};
-
-const clickStream = () => {
-    earnings += viewerInteraction;
-    updateEarningsDisplay();
-    saveGameData();
-};
-
-const upgradeCamera = () => {
-    const cameraCost = 10 * cameraQuality;
-    if (earnings >= cameraCost) {
-        earnings -= cameraCost;
-        cameraQuality++;
-        document.getElementById('cameraQuality').textContent = cameraQuality;
-        document.getElementById('cameraCost').textContent = (10 * cameraQuality).toFixed(2);
-        updateEarningsDisplay();
-        saveGameData();
-    } else {
-        alert("Not enough earnings to upgrade camera quality!");
-    }
-};
-
-const upgradeSoftware = () => {
-    const softwareCost = 50 * streamingSoftware;
-    if (earnings >= softwareCost) {
-        earnings -= softwareCost;
-        streamingSoftware++;
-        document.getElementById('streamingSoftware').textContent = streamingSoftware;
-        document.getElementById('softwareCost').textContent = (50 * streamingSoftware).toFixed(2);
-        updateEarningsDisplay();
-        saveGameData();
-    } else {
-        alert("Not enough earnings to upgrade streaming software!");
-    }
-};
-
-const upgradeInteraction = () => {
-    const interactionCost = 100 * viewerInteraction;
-    if (earnings >= interactionCost) {
-        earnings -= interactionCost;
-        viewerInteraction++;
-        document.getElementById('viewerInteraction').textContent = viewerInteraction;
-        document.getElementById('interactionCost').textContent = (100 * viewerInteraction).toFixed(2);
-        updateEarningsDisplay();
-        saveGameData();
-    } else {
-        alert("Not enough earnings to upgrade viewer interaction!");
-    }
-};
-
-const saveGameData = () => {
-    const gameData = {
-        earnings,
-        cameraQuality,
-        streamingSoftware,
-        viewerInteraction,
-        streamerName,
-    };
-    localStorage.setItem("idleStreamerData", JSON.stringify(gameData));
-};
-
-const loadGameData = (data) => {
-    earnings = data.earnings;
-    cameraQuality = data.cameraQuality;
-    streamingSoftware = data.streamingSoftware;
-    viewerInteraction = data.viewerInteraction;
-    streamerName = data.streamerName;
-    document.getElementById("streamerName").textContent = streamerName;
-    document.getElementById("streamerNameInput").value = streamerName;
-
-    updateEarningsDisplay();
-    document.getElementById('cameraQuality').textContent = cameraQuality;
-    document.getElementById('cameraCost').textContent = (10 * cameraQuality).toFixed(2);
-    document.getElementById('streamingSoftware').textContent = streamingSoftware;
-    document.getElementById('softwareCost').textContent = (50 * streamingSoftware).toFixed(2);
-    document.getElementById('viewerInteraction').textContent = viewerInteraction;
-    document.getElementById('interactionCost').textContent = (100 * viewerInteraction).toFixed(2);
-};
+// Start generating income every second
+setInterval(generateIncome, 1000);
